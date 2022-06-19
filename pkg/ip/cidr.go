c05ed46a3798b0a3f70b4b19b00c3318d40dbd29
Joe Stringer <joe@cilium.io>
2022-06-19 07:12:59 -0700

    ip: Add helpers to assist net -> netip transition
    These helpers can come in useful to convert between old and new IP types
    from the standard library in order to do incremental updates to
    individual packages without reworking IP address/prefix usage across
    the entire application all at once.
    
    Once we've fully migrated to netip, these should no longer be necessary
    and can be deleted again.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- pkg/ip/cidr.go
+++ pkg/ip/cidr.go
@@ -5,6 +5,7 @@ package ip
 
 import (
 	"net"
+	"net/netip"
 )
 
 // ParseCIDRs fetches all CIDRs referred to by the specified slice and returns
@@ -29,4 +30,31 @@ func ParseCIDRs(cidrs []string) (valid []*net.IPNet, invalid []string) {
 		}
 	}
 	return valid, invalid
+}
+
+// PrefixToIPNet is a convenience helper for migrating from the older 'net'
+// standard library types to the newer 'netip' types. Use this to plug the
+// new types in newer code into older types in older code during the migration.
+func PrefixToIPNet(prefix netip.Prefix) *net.IPNet {
+	return &net.IPNet{
+		IP:   prefix.Addr().AsSlice(),
+		Mask: net.CIDRMask(prefix.Bits(), prefix.Addr().BitLen()),
+	}
+}
+
+// IPNetToPrefix is a convenience helper for migrating from the older 'net'
+// standard library types to the newer 'netip' types. Use this to plug the
+// new types in newer code into older types in older code during the migration.
+func IPNetToPrefix(prefix *net.IPNet) netip.Prefix {
+	ip, _ := netip.AddrFromSlice(prefix.IP)
+	ones, _ := prefix.Mask.Size()
+	return netip.PrefixFrom(ip, ones)
+}
+
+// IPToNetPrefix is a convenience helper for migrating from the older 'net'
+// standard library types to the newer 'netip' types. Use this to plug the
+// new types in newer code into older types in older code during the migration.
+func IPToNetPrefix(ip net.IP) netip.Prefix {
+	a, _ := netip.AddrFromSlice(ip)
+	return netip.PrefixFrom(a, a.BitLen())
 }
